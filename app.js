1a429aebfbbdf3fbc3d47b6a23b2b27bb9701933
Informatica <informatica@alumno.local>
2024-05-30 11:43:22 -0600

    Conexion Successfully with Server

---
+++ app.js
@@ -0,0 +1,9 @@
+import { config } from 'dotenv'
+
+import Server from './configs/server.js'
+
+config()
+
+const server = new Server()
+
+server.listen()
---
+++ configs/server.js
@@ -0,0 +1,37 @@
+'use strict'
+
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+class Server{
+
+    constructor() {
+        
+        this.app = express()
+        this.port = process.env.PORT
+
+        this.middlewares()
+
+    }
+
+    middlewares() {
+        this.app.use(express.urlencoded({ extended: false }))
+        this.app.use(cors())
+        this.app.use(express.json())
+        this.app.use(helmet())
+        this.app.use(morgan('dev'))
+    }
+
+    listen() {
+        
+        this.app.listen(this.port, () => {
+            console.log('Server is running on port: ', this.port)
+        })
+        
+    }
+
+}
+
+export default Server
