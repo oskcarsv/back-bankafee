c2649684e896b66a3f8cfa5837deb77f27202718
oskcarsv <oskcarqsv@gmail.com>
2024-06-10 15:09:44 -0600

    finish favorite model

---
+++ src/favorite/favorite.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const favoriteSchema = mongoose.Schema({
+    noOwnerAccount: {
+        type: String,
+        required: [true, "No Account Owner is required"],
+    },
+
+    favorites: [
+        {
+            alias: {
+                type: String,
+                required: [true, "Alias of the account is required"],
+            },
+            noAccount: {
+                type: String,
+                required: [true, "Number of account is required"],
+            },
+        },
+    ],
+});
+
+export default mongoose.model("Favorite", favoriteSchema)
